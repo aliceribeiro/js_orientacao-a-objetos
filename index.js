91f9db02329b176b9d2692399907e81a976d04a6
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-09-05 14:55:55 -0300

    feat: :sparkles: implementa diversos tipos de contas

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-console.log(cliente1);
 
-const conta1 = new ContaCorrente(1001, cliente1);
+const conta1 = new ContaCorrente(cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
+
+console.log(contaPoupanca);
 console.log(conta1);
-
-conta1.depositar(500);
-console.log(conta1.saldo);
-
-conta1.sacar(100);
-console.log(conta1.saldo);
-
-const cliente2 = new Cliente("Alice", 88822233309);
-console.log(cliente2);
-const conta2 = new ContaCorrente(1002, cliente2);
-console.log(conta2);
-
-let valor = 150;
-conta1.transferir(valor, conta2);
-
-console.log(conta2.saldo);
-console.log(conta1.saldo);
-
-console.log(ContaCorrente.numeroDeContas)
+console.log(contaSalario);
